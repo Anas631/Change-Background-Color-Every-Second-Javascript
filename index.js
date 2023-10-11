40b875da4b6b35ae9810613750388a9f17282575
ANAS CHISHTY <87268800+Anas631@users.noreply.github.com>
2023-10-11 16:44:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const randomColor = function(){
-    let hex = '0123456789ABCDEF'
-    let color = '#'
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
     for (let i = 0; i < 6; i++) {
-        color += hex[Math.floor(Math.random() *16)]
+      color += hex[Math.floor(Math.random() * 16)];
     }
-
-    return color 
-    
-}
-
-let intervalId;
-
-function chengBgColor (){
-    document.body.style.backgroundColor= randomColor()
-}
-
-document.getElementById('start').addEventListener("click",function(){
-    intervalId =  setInterval(chengBgColor,1000)
-})
-
-document.getElementById("stop").addEventListener('click',function(){
-     clearInterval(intervalId)
-})
-
+    return color;
+  };
+  
+  let intervalId;
+  const startChangingColor = function () {
+    if (!intervalId) {
+      intervalId = setInterval(changeBgColor, 1000);
+    }
+  
+    function changeBgColor() {
+      document.body.style.backgroundColor = randomColor();
+    }
+  };
+  const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+  };
+  
+  document.querySelector('#start').addEventListener('click', startChangingColor);
+  
+  document.querySelector('#stop').addEventListener('click', stopChangingColor);
